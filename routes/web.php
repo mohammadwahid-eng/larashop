70f80cdf04362d32bf3d1c4ca8f35f951fb35381
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2022-03-23 17:10:43 +0600

    Default theme installed

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome";
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth'])->name('home');
+
+require __DIR__.'/auth.php';
